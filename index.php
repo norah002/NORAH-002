0662b8af6068256bfc57379fbf89a313decea03b
Norah Almajed <norah.almajed002@outlook.com>
2025-11-11 21:02:26 +0300

    Add sample user data to index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+[
+    {
+        "name": "نورة أحمد",
+        "phone": "0500000001",
+        "wants_drawing": 1,
+        "drawing_completed": 0,
+        "created_at": "2025-01-15 10:00:00",
+        "id": 1
+    },
+    {
+        "name": "فاطمة محمد",
+        "phone": "0500000002", 
+        "wants_drawing": 1,
+        "drawing_completed": 1,
+        "created_at": "2025-01-16 11:30:00",
+        "id": 2,
+        "hair_color": "أسود",
+        "eye_color": "بني",
+        "favorite_color": "#5999d9",
+        "skin_tone": "قمحي",
+        "hair_style": "طويل ومستقيم",
+        "clothing": "عباية سوداء",
+        "additional_notes": "أحب الألوان الزرقاء",
+        "photo_path": "",
+        "info_completed": 1,
+        "drawing_path": "drawings/sample_drawing.jpg"
+    }
+]
